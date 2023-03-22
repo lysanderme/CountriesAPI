13e2476dfd7593bcfd7d65f21c25fa5ed57b9bd3
Lysander Myrddin-Evans <myrddin-evans.l@etoncollege.org.uk>
2023-03-22 09:56:41 +0000

    Initial Commit

---
+++ CountriesAPI/CountriesAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesAPIApp.swift
+//  CountriesAPI
+//
+//  Created by Myrddin-Evans, Lysander (JD) on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CountriesAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
